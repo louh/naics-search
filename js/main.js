b100a83aed98d551128346c094e8aca82e5e4894
Lou Huang <lou@codeforamerica.org>
2013-09-13 13:14:14 -0700

    A more or less working bootstrap 3 version

--- js/main.js
+++ js/main.js
@@ -1,113 +1,122 @@
 // NAICS-API search example
-'use strict'
+'use strict';
 
 var naics
 var naicsAPI = 'http://api.naics.us/v0/q?year=2012&collapse=1&titlesonly=1'
 var naicsURL
 var naicsLocal,
-    naicsRemote
+	naicsRemote
 var naicsItem
 
 $(document).ready(function() {
 
-    // load NAICS
-    $.ajax({
-        url: naicsAPI,
-        async: false,
-        dataType: 'json',
-        success: function(data) {
-            naics = data;
-        },
-        failure: function(x) {
-            document.write('Error loading NAICS data')
-        }
-    });
+	// load NAICS
+	/*
+	$.ajax({
+		url: naicsAPI,
+		async: false,
+		dataType: 'json',
+		success: function(data) {
+			naics = data;
+		},
+		failure: function(x) {
+			document.write('Error loading NAICS data')
+		}
+	});*/
+	var naicsXHR = $.get(naicsAPI, function(data) {
+		naics = data
+	}).done( function () {
+		// format data
+		for (var i = 0; i < naics.length; i++) {
+			naics[i].id = naics[i].code
+		}
 
-    // SELECT2
+		$('#loading-msg').hide()
 
-    // format data
-    for (var i = 0; i < naics.length; i++) {
-        naics[i].id = naics[i].code
-    }
+		// select2 init
+		$('#naics-select').select2({
+			allowClear: true,
+			minimumInputLength: 1,
+			data: { results: naics, text: 'title' },
+			formatSelection: formatSelection,
+			formatResult: formatResult,
+			placeholder: 'Enter some keywords about your business',
+		}).select2('open')
 
-    // format selection things
-    function formatSelection(item) { return item.title }
-    function formatResult(item) { return '<small>' + item.code + '</small> <strong>' + item.title + '</strong>' }
+	}).fail( function () {
+		document.write('Error loading NAICS data')
+	})
 
-    // select2 init
-    $('#naics-select').select2({
-        allowClear: true,
-        minimumInputLength: 1,
-        data: { results: naics, text: 'title' },
-        formatSelection: formatSelection,
-        formatResult: formatResult,
-        placeholder: 'Enter some keywords about your business',
-    }).select2('open')
+	// SELECT2
 
-    $('#naics-select').on('select2-selecting', function (e) {
-        $('#naics-add').prop('disabled', false)
-    })
-    $('#naics-select').on('select2-removed', function (e) {
-        $('#naics-add').prop('disabled', true)
-    })
+	// format selection things
+	function formatSelection(item) { return item.title }
+	function formatResult(item) { return '<small>' + item.code + '</small> <strong>' + item.title + '</strong>' }
 
-    // INTERACTIONS
+	$('#naics-select').on('select2-selecting', function (e) {
+		$('#naics-add').prop('disabled', false)
+	})
+	$('#naics-select').on('select2-removed', function (e) {
+		$('#naics-add').prop('disabled', true)
+	})
 
-    // add a row
-    $('#naics-add').on('click', function (e) {
-        e.preventDefault()
-        addRow()
-    })
+	// INTERACTIONS
 
-    // delete a row
-    $('#naics-table').on('click', 'button.close', function() {
-        $(this).parent().parent().remove()
+	// add a row
+	$('#naics-add').on('click', function (e) {
+		e.preventDefault()
+		addRow()
+	})
 
-        if ($('tbody').html().trim() == '') {
-            $('#yes-table').hide()
-            $('#no-table').show()
-        }
+	// delete a row
+	$('#naics-table').on('click', 'button.close', function() {
+		$(this).parent().parent().remove()
 
-    })
+		if ($('tbody').html().trim() == '') {
+			$('#yes-table').hide()
+			$('#no-table').show()
+		}
 
-    // mark as primary
-    $('#naics-table').on('click', 'button.btn', function () {
-        if ($(this).hasClass('btn-success')) {
-            // toggle
-            $(this).removeClass('btn-success')
-        }
-        else {
-            $('#naics-table .btn').removeClass('btn-success')
-            $(this).addClass('btn-success')            
-        }
-    })
+	})
 
-    // view description on hover
-    $('#naicstable i').hover( function () {
+	// mark as primary
+	$('#naics-table').on('click', 'button.btn', function () {
+		if ($(this).hasClass('btn-success')) {
+			// toggle
+			$(this).removeClass('btn-success').text('Mark as primary')
+		}
+		else {
+			$('#naics-table .btn').removeClass('btn-success').text('Mark as primary')
+			$(this).addClass('btn-success').text('Primary')
+		}
+	})
 
-    })
+	// view description on hover
+	$('#naicstable i').hover( function () {
+
+	})
 
 })
 
 function addRow () {
-    if ($('#naics-select').select2('val') != '') {
-        var row = $('<tr></tr>').appendTo('tbody')
+	if ($('#naics-select').select2('val') != '') {
+		var row = $('<tr></tr>').appendTo('tbody')
 
-        var title = $('#naics-select').select2('data').title
-        var code = $('#naics-select').select2('val')
+		var title = $('#naics-select').select2('data').title
+		var code = $('#naics-select').select2('val')
 
-        row.html('<td class="col-md-1"><button class="close text-left">&times;</button></td><td class="col-md-10 text-left"><span> <i class="icon-eye-open"></i> <strong>' + title + '</strong> <small>' + code +'</small></span></td><td class="col-md-1"><button class="btn btn-small">Primary</button></td>')            
-    }
+		row.html('<td style="width: 60px"><button class="close pull-left">&times;</button></td><td class="text-left"><span class="glyphicon glyphicon-eye-open"></span> <strong>' + title + '</strong> <small>' + code +'</small></td><td class="text-right" style="width: 160px"><button class="btn btn-small btn-default">Mark as primary</button></td>')            
+	}
 
-    // set instructions in the table below
-    if ($('#no-table').is(":visible")) {
-        $('#no-table').hide()
-        $('#yes-table').show()
-    }
+	// set instructions in the table below
+	if ($('#no-table').is(":visible")) {
+		$('#no-table').hide()
+		$('#yes-table').show()
+	}
 
-    // Reset form
-    $('#naics-add').prop('disabled', true)
-    $('#naics-select').select2('val', '').select2('open')
+	// Reset form
+	$('#naics-add').prop('disabled', true)
+	$('#naics-select').select2('val', '').select2('open')
 }
 
 
